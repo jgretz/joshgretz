0acb0cb27fdb052fc324e2e1117da3277f52d39d
Josh Gretz <jgretz@truefit.io>
2018-05-22 23:33:51 -0400

    add about

--- site/app/features/blog/components/blog.js
+++ site/app/features/blog/components/blog.js
@@ -1,3 +1,3 @@
 import React from 'react';
 
-export default () => <div>Blog</div>;
+export default () => <div>Coming Soon :)</div>;
--- site/app/features/resume/components/resume.js
+++ site/app/features/resume/components/resume.js
@@ -1,3 +1,3 @@
 import React from 'react';
 
-export default () => <div>Resume</div>;
+export default () => <div>Coming Soon :)</div>;
