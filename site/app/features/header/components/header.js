032f7f963fcd080b697f0b941f7cb0ed8f7fa9af
Josh Gretz <jgretz@truefit.io>
2018-06-13 16:15:36 -0400

    remove some dead weight

--- site/app/features/header/components/header.js
+++ site/app/features/header/components/header.js
@@ -4,9 +4,6 @@ import Menu from './menu';
 export default () => (
   <Fragment>
     <Menu />
-    <div className="hero">
-      <h1>Josh Gretz</h1>
-      <h2>Musings of a Maker</h2>
-    </div>
+    <div className="hero" />
   </Fragment>
 );
