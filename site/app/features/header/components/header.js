a3fbd6cc8a2f4566a6ac6472f0bf06078f33a87d
Josh Gretz <jgretz@truefit.io>
2018-06-13 16:08:25 -0400

    fix some seo stuff

--- site/app/features/header/components/header.js
+++ site/app/features/header/components/header.js
@@ -4,6 +4,9 @@ import Menu from './menu';
 export default () => (
   <Fragment>
     <Menu />
-    <div className="hero" />
+    <div className="hero">
+      <h1>Josh Gretz</h1>
+      <h2>Musings of a Maker</h2>
+    </div>
   </Fragment>
 );
