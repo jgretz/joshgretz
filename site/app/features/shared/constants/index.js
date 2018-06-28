89848865bc7a7076667542eaf88681f452780baf
Josh Gretz <jgretz@truefit.io>
2018-06-28 14:52:46 -0400

    update description

--- site/app/features/shared/constants/index.js
+++ site/app/features/shared/constants/index.js
@@ -5,5 +5,8 @@ export const ROUTES = {
 };
 
 export const GENERIC_TITLE = 'Musings of a Maker';
-export const GENERIC_DESC = 'Josh Gretz is maker living in Pittsburgh. This site contains his musings, thoughts, and learnings earned through his tinkering and career.';
+export const GENERIC_DESC = `Josh Gretz is an experienced developer with a demonstrated history of creating flexible architectures, crafting clean code, and
+leading teams to produce software products people love. Josh Gretz strongly believes that the best software is built by people
+for people, that software development is artistic endeavor rooted in engineering sensibility, and that code can be an
+artistic medium. Josh Gretz has strong opinions, but tries to hold them loosely. Josh Gretz desires to continually improve.`;
 export const GENERIC_IMG = 'https://i.imgur.com/FrQNZnN.jpg';
