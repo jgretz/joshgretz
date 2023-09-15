87a68c852d05c05108eeb75fdabb740796ffd77b
Josh Gretz <jgretz@truefit.io>
2023-09-15 12:49:27 -0400

    update card with strava icon and remove facebook and twitter

--- site/app/components/card.tsx
+++ site/app/components/card.tsx
@@ -1,6 +1,7 @@
 import {Link} from '@remix-run/react';
-import {Facebook, Github, Instagram, Linkedin, Twitter} from 'lucide-react';
+import {Github, Instagram, Linkedin} from 'lucide-react';
 import avatar from '~/assets/images/josh-g.png';
+import strava from '~/assets/images/strava-icon.png';
 
 export default function Card() {
   return (
@@ -12,20 +13,17 @@ export default function Card() {
       />
       <h1 className="text-center w-full mt-5 text-4xl">Josh Gretz</h1>
       <div className="flex flex-row justify-center py-3">
-        <Link to="https://www.facebook.com/josh.gretz.3/" target="_blank">
-          <Facebook className="mx-2" />
-        </Link>
-        <Link to="https://www.instagram.com/joshgretz/" target="_blank">
-          <Instagram className="mx-2" />
-        </Link>
-        <Link to="https://twitter.com/joshgretz" target="_blank">
-          <Twitter className="mx-2" />
+        <Link to="https://www.linkedin.com/in/joshgretz/" target="_blank">
+          <Linkedin className="mx-2" />
         </Link>
         <Link to="https://github.com/jgretz" target="_blank">
           <Github className="mx-2" />
         </Link>
-        <Link to="https://www.linkedin.com/in/joshgretz/" target="_blank">
-          <Linkedin className="mx-2" />
+        <Link to="https://www.instagram.com/joshgretz/" target="_blank">
+          <Instagram className="mx-2" />
+        </Link>
+        <Link to="https://www.strava.com/athletes/67556995" target="_blank">
+          <img src={strava} alt="strava icon" className="h-[24px] w-[24px] bg-black" />
         </Link>
       </div>
     </div>
