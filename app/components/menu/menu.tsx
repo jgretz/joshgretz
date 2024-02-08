4b5fd2e041f5fa528476eddf1ad037def8cf2032
Josh Gretz <jgretz@truefit.io>
2024-02-08 16:27:38 -0500

    cleanup for publish

--- app/components/menu/menu.tsx
+++ app/components/menu/menu.tsx
@@ -5,7 +5,7 @@ export default function Menu() {
     <ul className="flex w-2/3 flex-row items-center justify-center sm:flex-col">
       <MenuItem to="/">About</MenuItem>
       <MenuItem to="/resume">Resume</MenuItem>
-      <MenuItem to="/running">Running</MenuItem>
+      {/* <MenuItem to="/running">Running</MenuItem> */}
       {/* <MenuItem to="/thoughts">Thoughts</MenuItem> */}
     </ul>
   );
