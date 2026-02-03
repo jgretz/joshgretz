aabe4da0b0c5346c55e48027191739e54f3117ac
Josh Gretz <jgretz@gmail.com>
2026-02-03 17:37:20 -0500

    fix(www): update cathedral link to be a better one

--- apps/www/src/routes/readme.tsx
+++ apps/www/src/routes/readme.tsx
@@ -80,7 +80,7 @@ const books = [
   {
     title: 'The Cathedral and the Bazaar',
     author: 'Eric S. Raymond',
-    href: 'https://www.catb.org/~esr/writings/cathedral-bazaar/cathedral-bazaar/',
+    href: 'https://monoskop.org/images/e/e0/Raymond_Eric_S_The_Cathedral_and_the_Bazaar_rev_ed.pdf',
   },
 ] as const;
 
